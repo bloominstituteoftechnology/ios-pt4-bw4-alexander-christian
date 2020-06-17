9c89a6c27970d147f4138b26977fd3c6ba2eec25
Christian Lorenzo <clbmiami2004@gmail.com>
2020-06-16 22:24:01 -0700

    Created code on Scene Delegate for Onboarding Screens. Created 2 more files OnboardingTwoViewController, and OnboardingThreeViewController.

--- Vortex Mortgage/Vortex Mortgage/SceneDelegate.swift
+++ Vortex Mortgage/Vortex Mortgage/SceneDelegate.swift
@@ -17,7 +17,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        //Onboarding Screen Code:
+        let launchedBefore = UserDefaults.standard.bool(forKey: "hasLaunched")
+        let launhStoryBoard = (UIStoryboard(name: "Onboarding", bundle: nil))
+        let mainStoryBoard = UIStoryboard(name: "Main", bundle: nil)
+        var vc: UIViewController
+        
+        if launchedBefore {
+            vc = mainStoryBoard.instantiateInitialViewController()!
+        } else {
+            vc = launhStoryBoard.instantiateViewController(identifier: "hasLaunched")
+        }
+        
+        UserDefaults.standard.set(true, forKey: "hasLaunched")
+        self.window?.rootViewController = vc
+        
+        //guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

0cf56ed2f65a7aacb56c758d49dc3f14815c76a0
Christian Lorenzo <clbmiami2004@gmail.com>
2020-06-18 21:11:50 -0700

    Calculations Done!

---
+++ Vortex Mortgage/Calculation Controllers/ResultsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ResultsViewController.swift
+//  Vortex Mortgage
+//
+//  Created by Christian Lorenzo on 6/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var totalAmount: Float?
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //NotificationCenter.default.addObserver(self, selector: #selector(didGetNotification(_:)), name: Notification.Name("Result"), object: nil)
+        if let total = totalAmount {
+            resultLabel.text = "$\(total)"
+        }
+        
+    }
+    
+    @objc func didGetNotification(_ notification: Notification) {
+        let totalAmount = notification.object as! String?
+        resultLabel.text = totalAmount
+    }
+    
+    
+    @IBAction func recalculateButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}

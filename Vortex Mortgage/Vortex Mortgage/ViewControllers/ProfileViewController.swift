931213aaf5d39b8f9221abc73d789f4dc186a9dc
Alex Thompson <guitarman226@gmail.com>
2020-06-27 18:34:58 -0500

    Fixed profile page bugs

--- Vortex Mortgage/Vortex Mortgage/ViewControllers/ProfileViewController.swift
+++ Vortex Mortgage/Vortex Mortgage/ViewControllers/ProfileViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import AuthenticationServices
 
 class ProfileViewController: UIViewController {
     
@@ -16,50 +15,123 @@ class ProfileViewController: UIViewController {
     var email: String?
     var first: String?
     var last: String?
+    var phoneNumber: String?
+    var location: String?
     
     //MARK: Outlets
     
     @IBOutlet var firstName: UILabel!
     @IBOutlet var lastName: UILabel!
     @IBOutlet var emailLabel: UILabel!
-    @IBOutlet var id: UILabel!
     @IBOutlet var profileView: UIView!
     @IBOutlet var profileImage: UIView!
     @IBOutlet var phoneNumberLabel: UILabel!
     @IBOutlet var phoneNumberTextField: UITextField!
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var phoneImage: UIImageView!
+    @IBOutlet var locationImage: UIImageView!
+    @IBOutlet var cancelButton: UIButton!
     
     
     //MARK: View Controller life cycle
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstName.text = first
-        lastName.text = last
-        emailLabel.text = email
+        updateViews()
+        addUITweaks()
+    }
+    
+    func updateViews() {
+        guard isViewLoaded else { return }
+        self.emailLabel.text = email
+        self.firstName.text = first
+        self.lastName.text = last
+    }
+    
+    func addUITweaks() {
         emailLabel.layer.borderWidth = 0.5
         emailLabel.layer.borderColor = UIColor.black.cgColor
         emailLabel.layer.cornerRadius = 5
         profileImage.layer.cornerRadius = 20
         profileView.layer.cornerRadius = 38
-        
+        saveButton.layer.cornerRadius = 10
+        cancelButton.layer.cornerRadius = 10
+        saveButton.layer.shadowColor = UIColor.black.cgColor
+        saveButton.layer.shadowOffset = .zero
+        saveButton.layer.shadowRadius = 10
+        saveButton.layer.shadowOpacity = 0.5
+        cancelButton.layer.shadowColor = UIColor.black.cgColor
+        cancelButton.layer.shadowOffset = .zero
+        cancelButton.layer.shadowRadius = 10
+        cancelButton.layer.shadowOpacity = 0.5
     }
-    
     //MARK: Actions
     
     @IBAction func editProfileTapped(_ sender: Any) {
+
+        locationLabel.isHidden = true
+        locationTextField.isHidden = false
+        phoneNumberTextField.isHidden = false
+        locationImage.isHidden = false
+        phoneImage.isHidden = false
+        saveButton.isHidden = false
+        cancelButton.isHidden = false
+        
+        if phoneNumberTextField.isHidden == false {
+            phoneNumberLabel.isHidden = true
+        } else {
+            phoneNumberLabel.isHidden = false
+        }
+    }
+    
+    @IBAction func cancelTapped(_ sender: Any) {
+        
+        locationTextField.isHidden = true
+        phoneNumberTextField.isHidden = true
+        if phoneNumberTextField.text == "" {
+            phoneImage.isHidden = true
+        } else  {
+            phoneImage.isHidden = false
+            phoneNumberLabel.isHidden = false
+        }
+        if locationTextField.text == "" {
+            locationImage.isHidden = true
+        } else {
+            locationImage.isHidden = false
+            locationLabel.isHidden = false
+        }
+        saveButton.isHidden = true
+        cancelButton.isHidden = true
+        
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        phoneNumber = phoneNumberTextField.text
+        location = locationTextField.text
+        locationLabel.text = location
+        phoneNumberLabel.text = phoneNumber
+        phoneNumberTextField.isHidden = true
+        locationTextField.isHidden = true
+        cancelButton.isHidden = true
+        saveButton.isHidden = true
+        phoneNumberLabel.isHidden = false
+        locationLabel.isHidden = false
+        
+        if phoneNumber == "" {
+            phoneImage.isHidden = true
+        } else {
+            phoneImage.isHidden = false
+        }
+        
+        if location == "" {
+            locationImage.isHidden = true
+        } else {
+            locationImage.isHidden = false
+        }
     }
-    */
-
 }

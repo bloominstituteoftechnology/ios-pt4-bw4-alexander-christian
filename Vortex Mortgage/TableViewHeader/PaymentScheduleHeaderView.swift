e118d5e1ac8cac24899cfc4929cc345f30d5b6b6
Christian Lorenzo <clbmiami2004@gmail.com>
2020-06-29 20:07:50 -0700

    Created a HeaderFooterView file

---
+++ Vortex Mortgage/TableViewHeader/PaymentScheduleHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  PaymentScheduleHeaderView.swift
+//  Vortex Mortgage
+//
+//  Created by Christian Lorenzo on 6/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PaymentScheduleHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
